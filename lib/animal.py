671353460290f6f6cc3fadb921661e200c0c02f6
Anthony Bosek <anthony.bosek@gmail.com>
2023-10-27 10:15:58 -0700

    include basic inits

--- lib/animal.py
+++ lib/animal.py
@@ -38,3 +38,13 @@ class Animal:
     @classmethod
     def find_by_species(cls, spec):
         return [anml.nickname for anml in cls.all if anml.species == spec]
+
+
+"""
+    def __init__( self, species, weight, nickname, zoo_instance ):
+        self.species = species
+        self.weight = weight
+        self.nickname = nickname
+        self.zoo = zoo_instance
+        Animal.all.append( self )
+"""
